ab8ab8e6c28ed0745b91a705acb9dc0def2b5869
Sungjae Jung <sungjae.2425@gmail.com>
2018-07-30 22:28:53 -0700

    move index.js under src/

--- index.js
+++
@@ -1,9 +0,0 @@
-import Utils from './lib/utils';
-import Dicom from './lib/dicom/dicom';
-import Fhir from './lib/fhir/stu3';
-import Merkle from './lib/merklize/merkle-patricia-tree';
-
-export const utils = Utils;
-export const dicom = Dicom;
-export const fhir = Fhir;
-export const merkle = Merkle;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Utils from './utils';
+import Dicom from './dicom/dicom';
+import Fhir from './fhir/stu3';
+import Merkle from './merklize/merkle-patricia-tree';
+
+export const utils = Utils;
+export const dicom = Dicom;
+export const fhir = Fhir;
+export const merkle = Merkle;
