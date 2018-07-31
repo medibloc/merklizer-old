ee66f484c5405bca74d58668ea6bc29c144faea7
Sungjae Jung <sungjae.2425@gmail.com>
2018-07-31 02:47:50 -0700

    update example

--- example/example.js
+++ example/example.js
@@ -8,16 +8,16 @@ fs.readFile(condition_example, 'utf8', (e,r) => {
   if (e) throw e;
 
   let condition = JSON.parse(r);
-  console.log('read: ', condition);
+  // console.log('read: ', condition);
 
   var plain = fhir.filter(condition);
-  console.log('plain: ', plain);
+  // console.log('plain: ', plain);
 
   var flat = utils.flat(plain);
-  console.log('flat: ', flat);
+  // console.log('flat: ', flat);
 
   var arr = utils.objectToArray(flat);
-  console.log('array: ', arr);
+  // console.log('array: ', arr);
 
   merkle.createTrie(arr, (e,r) => {
     var trie = r;
